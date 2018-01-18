b4d7462e2e743b673ec3c310391507f7b94db282
DennisVis <dennisvis86@gmail.com>
2018-01-18 22:10:22 +0100

    Added pgp related config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$TRAVIS_TAG" ]
-then
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == "false" ]]; then
+  if [ ! -z "$TRAVIS_TAG" ]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings deploy/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
-else
+    mvn --settings $GPG_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+  else
     echo "not on a tag -> keep snapshot version in pom.xml"
-fi
+  fi
 
-mvn clean deploy --settings deploy/settings.xml -DskipTests=true -B -U
+  mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+  exit $?
+fi
