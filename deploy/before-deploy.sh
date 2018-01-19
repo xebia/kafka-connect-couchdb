8f8d62994eecbd1173aaa808c6f01358c7fb469c
DennisVis <dennisvis86@gmail.com>
2018-01-19 11:50:02 +0100

    Fixed keyring name in before-deploy script

--- deploy/before-deploy.sh
+++ deploy/before-deploy.sh
@@ -3,5 +3,5 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_97c7a3c439c7_key -iv $encrypted_97c7a3c439c7_iv \
       -in $DEPLOY_DIR/codesigning.asc.enc -out $DEPLOY_DIR/codesigning.asc -d
-    gpg --fast-import $DEPLOY_DIR/signingkey.asc
+    gpg --fast-import $DEPLOY_DIR/codesigning.asc
 fi
