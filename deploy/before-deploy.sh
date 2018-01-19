21d4aa58d14a9e304d93533f12d5476804371978
DennisVis <dennisvis86@gmail.com>
2018-01-19 11:34:08 +0100

    Attempt to fix deploy config

---
+++ deploy/before-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_97c7a3c439c7_key -iv $encrypted_97c7a3c439c7_iv -in $DEPLOY_DIR/codesigning.asc.enc -out $DEPLOY_DIR/codesigning.asc -d
+    gpg --fast-import $DEPLOY_DIR/signingkey.asc
+fi
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   if [ ! -z "$TRAVIS_TAG" ]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings $GPG_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings $DEPLOY_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
   else
     echo "not on a tag -> keep snapshot version in pom.xml"
   fi
 
-  mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-  exit $?
+  mvn deploy --settings $DEPLOY_DIR/settings.xml -DperformRelease=true -DskipTests=true
 fi
